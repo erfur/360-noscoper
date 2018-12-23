f8ec2e164765e044888c9fa4e2e1400fdc92b770
erfur <erfur@itu.edu.tr>
2018-12-23 15:26:21 +0300

    added multithreading

--- python/360-noscoper.py
+++ python/360-noscoper.py
@@ -4,24 +4,31 @@ import sys
 from binascii import hexlify
 import asyncio, evdev
 import configparser
+import simpleaudio as sa
+import concurrent.futures
 
-def playFile(fileName):
-    print("playing {}".format(fileName))
+def playFile(file):
+    print("playing {}".format(file))
+    wav = sa.WaveObject.from_wave_file(file)
+    play = wav.play()
+    play.wait_done()
 
 def handleKey(keycode):
+    print(keycode)
     if isinstance(keycode, list) and keycode[0] in bindings.keys():
-        playFile(bindings[keycode[0]])
+        playFile(bindings['folder']+bindings[keycode[0]])
     elif keycode in bindings.keys():
-        playFile(bindings[keycode])
+        playFile(bindings['folder']+bindings[keycode])
     else:
-        playFile(bindings['default'])
+        playFile(bindings['folder']+bindings['default'])
 
 async def filterKeyEvents(device):
     async for event in device.async_read_loop():
         evt = evdev.categorize(event)
         if isinstance(evt, evdev.events.KeyEvent):
-            if evt.key_down:
-                handleKey(evdev.categorize(event).keycode)
+            # print(evt.keystate)
+            if evt.keystate == 1:
+                executor.submit(handleKey, evdev.categorize(event).keycode)
 
 def devicesWithButtons():
     return [evdev.InputDevice(path) for path in evdev.list_devices()\
@@ -33,6 +40,10 @@ def getConfig(file='config.ini'):
     config.read(file)
     bindings = config['bindings']
 
+def setWorkers():
+    global executor
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+
 def main(args):
     for device in devicesWithButtons():
         asyncio.ensure_future(filterKeyEvents(device))
@@ -42,5 +53,6 @@ def main(args):
 
 if __name__ == "__main__":
     getConfig()
+    setWorkers()
     # with daemon.DaemonContext():
     main(sys.argv[1:])
