f8f248c8717c9bbf5eb4aa0f04bc2522efc35197
erfur <erfur@itu.edu.tr>
2018-12-23 06:12:55 +0300

    implemented daemon, asyncio, evdev

--- python/360-noscoper.py
+++ python/360-noscoper.py
@@ -1,15 +1,32 @@
-import daemon
+#!/usr/bin/python
+#import daemon
 import sys
+from binascii import hexlify
+import asyncio, evdev
 
 def playFile():
     pass
 
-def readInput():
+def handleKey():
     pass
+
+async def print_events(device):
+    async for event in device.async_read_loop():
+        evt = evdev.categorize(event)
+        if isinstance(evt, evdev.events.KeyEvent):
+            if evt.key_down:
+                handleKey()
 
 def main(args):
-    pass
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()\
+                if ('EV_KEY', 1) in evdev.InputDevice(path).capabilities(verbose=True)]
+
+    for device in devices:
+        asyncio.ensure_future(print_events(device))
+    
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
 
 if __name__ == "__main__":
-    with daemon.DaemonContext():
-        main(sys.argv[1:])
+    # with daemon.DaemonContext():
+    main(sys.argv[1:])
