7f5958789c25e4d102629d4993a6cc1261bb4724
erfur <erfur@itu.edu.tr>
2018-12-23 14:57:18 +0300

    implemented key handling and configuration management

--- python/360-noscoper.py
+++ python/360-noscoper.py
@@ -3,30 +3,44 @@
 import sys
 from binascii import hexlify
 import asyncio, evdev
+import configparser
 
-def playFile():
-    pass
+def playFile(fileName):
+    print("playing {}".format(fileName))
 
-def handleKey():
-    pass
+def handleKey(keycode):
+    if isinstance(keycode, list) and keycode[0] in bindings.keys():
+        playFile(bindings[keycode[0]])
+    elif keycode in bindings.keys():
+        playFile(bindings[keycode])
+    else:
+        playFile(bindings['default'])
 
-async def print_events(device):
+async def filterKeyEvents(device):
     async for event in device.async_read_loop():
         evt = evdev.categorize(event)
         if isinstance(evt, evdev.events.KeyEvent):
             if evt.key_down:
-                handleKey()
+                handleKey(evdev.categorize(event).keycode)
 
-def main(args):
-    devices = [evdev.InputDevice(path) for path in evdev.list_devices()\
+def devicesWithButtons():
+    return [evdev.InputDevice(path) for path in evdev.list_devices()\
                 if ('EV_KEY', 1) in evdev.InputDevice(path).capabilities(verbose=True)]
 
-    for device in devices:
-        asyncio.ensure_future(print_events(device))
+def getConfig(file='config.ini'):
+    global bindings
+    config = configparser.ConfigParser()
+    config.read(file)
+    bindings = config['bindings']
+
+def main(args):
+    for device in devicesWithButtons():
+        asyncio.ensure_future(filterKeyEvents(device))
     
     loop = asyncio.get_event_loop()
     loop.run_forever()
 
 if __name__ == "__main__":
+    getConfig()
     # with daemon.DaemonContext():
     main(sys.argv[1:])
